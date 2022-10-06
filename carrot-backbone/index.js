c367d5b5a0793ed26fdd2635f5baadafbad0c8a8
J.Reczko <reczko.jakub@gmail.com>
2022-10-06 18:38:21 +0200

    Add body parser to the list of dependencies

--- carrot-backbone/index.js
+++ carrot-backbone/index.js
@@ -1,12 +1,15 @@
 'use strict';
 const express = require('express');
+const bodyParser = require('body-parser')
+const app = express();
+
+app.use(bodyParser.json())
 
 // Constants
 const PORT = process.env.PORT || 5000;
 const HOST = '0.0.0.0';
 
 // App
-const app = express();
 app.get('/', (req, res) => {
     res.json({ message: 'ok' });
 });
