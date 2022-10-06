75920b54ba809329bb815c3d2e96621f9729801a
J.Reczko <reczko.jakub@gmail.com>
2022-10-06 18:00:27 +0200

    Add Dockerfile to the backbone and primitive express server app

---
+++ carrot-backbone/index.js
@@ -0,0 +1,15 @@
+'use strict';
+const express = require('express');
+
+// Constants
+const PORT = process.env.PORT || 5000;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+    res.json({ message: 'ok' });
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
