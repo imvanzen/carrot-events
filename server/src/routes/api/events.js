54fd2c84e829755843d772b4f46f43716db4e279
J.Reczko <reczko.jakub@gmail.com>
2022-10-09 18:11:02 +0200

    Add schemas for request validation

--- server/src/routes/api/events.js
+++ server/src/routes/api/events.js
@@ -1,16 +1,38 @@
-const express = require('express')
+const express = require('express');
+const Joi = require('joi');
 const router = express.Router()
+const validator = require('../../utils/validator')
 const eventController = require('../../controllers/events')
+
+const paramsSchema = (
+    Joi.object().keys({
+        eventId: Joi.required().guid(),
+    })
+)
+
+const bodySchema = (
+    Joi.object().keys({
+        first_name: Joi.string().required(),
+        last_name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        event_date: Joi.string().required()
+    })
+)
+
+const createEvent = { body: bodySchema }
+const getEvent = { params: paramsSchema }
+const updateEvent = { params: bodySchema, body: bodySchema }
+const deleteEvent = { params: bodySchema }
 
 router
     .route('/')
     .get(eventController.getEvents)
-    .post(eventController.createEvent)
+    .post(validator(createEvent), eventController.createEvent)
 
 router
     .route('/:eventId')
-    .get(eventController.getEvent)
-    .put(eventController.updateEvent)
-    .delete(eventController.deleteEvent)
+    .get(validator(getEvent), eventController.getEvent)
+    .put(validator(updateEvent), eventController.updateEvent)
+    .delete(validator(deleteEvent), eventController.deleteEvent)
 
 module.exports = router
