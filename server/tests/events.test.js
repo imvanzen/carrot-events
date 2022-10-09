58e5ee9b074cde69583f1d3bbb82d1b5478021fb
J.Reczko <reczko.jakub@gmail.com>
2022-10-09 12:10:50 +0200

    Base test case for events

---
+++ server/tests/events.test.js
@@ -0,0 +1,31 @@
+const request = require('supertest')
+const httpStatus = require('http-status')
+const app = require('../src/app')
+
+
+
+describe('Event routes', () => {
+    let server, agent;
+
+    beforeEach(done => {
+        server = app.listen(5001, err => {
+            if (err) return done(err)
+            agent = request.agent(server)
+            done()
+        })
+    })
+
+    afterEach(done => {
+        if (!server) return done()
+        server.close(done)
+    })
+
+    test('should return 200 and list of events', async () => {
+        const req = await agent
+            .get('/v1/events')
+            .send()
+            .expect(httpStatus.OK)
+
+        expect(req.body).toEqual([{ id: 1 }, { id: 2 }])
+    })
+})
