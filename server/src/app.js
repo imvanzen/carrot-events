3cea35fde64dac4c1d6df128e1c504e3e7635f95
J.Reczko <reczko.jakub@gmail.com>
2022-10-09 17:04:56 +0200

    Catch API errors and wrap them in ApiError

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const routes = require('./routes')
+const httpStatus = require('http-status')
+const ApiError = require('./utils/ApiError')
 
 const app = express()
 
@@ -10,5 +12,9 @@ app.use(express.urlencoded({ extended: true }))
  * v1 Routes
  */
 app.use('/v1', routes)
+
+app.use((req, res, next) => {
+    next(new ApiError(httpStatus.NOT_FOUND, 'Not found'))
+})
 
 module.exports = app
