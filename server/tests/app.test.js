74d42370c873d39ac46395e1414bdefb8eb19a83
J.Reczko <reczko.jakub@gmail.com>
2022-10-09 11:30:39 +0200

    Setup server for tests, correct assertion

--- server/tests/app.test.js
+++ server/tests/app.test.js
@@ -1,11 +1,27 @@
-const supertest = require('supertest')
+const request = require('supertest')
+const httpStatus = require('http-status')
 const app = require('../src/app')
 
-
 describe('App', () => {
+    let server, agent;
+
+    beforeEach(done => {
+        server = app.listen(5001, err => {
+            if (err) return done(err)
+            agent = request.agent(server)
+            done()
+        })
+    })
+
+    afterEach(done => {
+        if (!server) return done()
+        server.close(done)
+    })
+
     test('Does startup', async () => {
-        const req = request(app)
+        const req = await agent
             .get('/')
-            .expect('200')
+            .send()
+            .expect(httpStatus.OK)
     })
 })
