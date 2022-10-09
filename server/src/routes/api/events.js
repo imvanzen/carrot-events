0c8d54bb7f8d6561d02968f6a6f063a0ade3816c
J.Reczko <reczko.jakub@gmail.com>
2022-10-09 12:24:37 +0200

    Get event by id route

--- server/src/routes/api/events.js
+++ server/src/routes/api/events.js
@@ -3,5 +3,6 @@ const router = express.Router()
 
 router
     .get('/', (req, res) => res.json([{ id: 1 }, { id: 2 }]))
+    .get('/:eventId', (req, res) => res.json({ id: req.params.eventId }))
 
 module.exports = router
