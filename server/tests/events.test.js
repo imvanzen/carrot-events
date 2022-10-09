69ff5009c8344a7a9c1795ee320c386f9c4fb618
J.Reczko <reczko.jakub@gmail.com>
2022-10-09 13:35:00 +0200

    Test for modification routes

--- server/tests/events.test.js
+++ server/tests/events.test.js
@@ -1,11 +1,22 @@
 const request = require('supertest')
 const httpStatus = require('http-status')
+const { faker } = require('@faker-js/faker')
 const app = require('../src/app')
 
 describe('Event routes', () => {
-    let server, agent;
+    let server, agent, newEvent
 
     beforeEach(done => {
+        const firstName = faker.name.firstName()
+        const lastName = faker.name.lastName()
+
+        newEvent = {
+            firstName,
+            lastName,
+            email: faker.internet.email(firstName, lastName).toLowerCase(),
+            eventDate: faker.date.soon()
+        }
+
         server = app.listen(5001, err => {
             if (err) return done(err)
             agent = request.agent(server)
@@ -28,7 +39,7 @@ describe('Event routes', () => {
     })
 
     test('should return 200 and event by ID', async () => {
-        const userId = "123";
+        const eventId = "123"
         const req = await agent
             .get(`/v1/events/${userId}`)
             .send()
@@ -38,10 +49,55 @@ describe('Event routes', () => {
     })
 
     test('should return 404 and no event by ID', async () => {
-        const userId = "123";
+        const eventId = "123"
         const req = await agent
             .get(`/v1/events/${userId}`)
             .send()
             .expect(httpStatus.NOT_FOUND)
+    })
+
+    test('should return 202 and create an event', async () => {
+        const req = await agent
+            .post(`/v1/events`)
+            .send(newEvent)
+            .expect(httpStatus.ACCEPTED)
+    })
+
+    test('should return 400 when creating event but any param in missed', async () => {
+        await agent
+            .post(`/v1/events`)
+            .send({ ...newEvent, firstName: undefined })
+            .expect(httpStatus.BAD_REQUEST)
+
+        await agent
+            .post(`/v1/events`)
+            .send({ ...newEvent, lastName: undefined })
+            .expect(httpStatus.BAD_REQUEST)
+
+        await agent
+            .post(`/v1/events`)
+            .send({ ...newEvent, email: undefined })
+            .expect(httpStatus.BAD_REQUEST)
+
+        await agent
+            .post(`/v1/events`)
+            .send({ ...newEvent, eventDate: undefined })
+            .expect(httpStatus.BAD_REQUEST)
+    })
+
+    test('should return 400 when creating an event but email format is in bad format', async () => {
+        newEvent.email = "Bad format email"
+        const req = await agent
+            .post(`/v1/events`)
+            .send(newEvent)
+            .expect(httpStatus.BAD_REQUEST)
+    })
+
+    test('should return 400 when creating an event but date is in bad format', async () => {
+        newEvent.eventDate = "Bad format event date"
+        const req = await agent
+            .post(`/v1/events`)
+            .send(newEvent)
+            .expect(httpStatus.BAD_REQUEST)
     })
 })
