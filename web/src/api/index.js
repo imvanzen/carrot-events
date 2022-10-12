b3140dbf788e3624abf1a8bae09cd1f984ab45a4
J.Reczko <reczko.jakub@gmail.com>
2022-10-12 19:37:44 +0200

    Fix env vars for web

--- web/src/api/index.js
+++ web/src/api/index.js
@@ -1,9 +1,9 @@
 import axios from "axios"
 
-const API_URL = process.env.API_URL
+const API_URL = process.env.REACT_APP_API_URL
 
 console.log(API_URL)
 
 export default axios.create({
-    baseURL: API_URL
+    baseURL: `http://${API_URL}`
 })
