a3edbe5b50c12008360ed9791009dc5368039330
J.Reczko <reczko.jakub@gmail.com>
2022-10-14 22:16:58 +0200

    Cleanup ApiError Handler

--- server/src/utils/ApiError.js
+++ server/src/utils/ApiError.js
@@ -1,12 +1,11 @@
 class ApiError extends Error {
-    constructor(statusCode, message, isOperational = true, stack = '') {
+    constructor(statusCode, message, stack = '') {
         super(message)
         this.statusCode = statusCode
-        this.isOperational = isOperational
         if (stack) {
             this.stack = stack
         } else {
-            Error.captureStackTrace(this, this.constructor)
+            this.stack = Error.captureStackTrace(this, this.constructor)
         }
     }
 }
