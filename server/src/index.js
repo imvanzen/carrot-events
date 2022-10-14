d12acf022a31f3c1f1b12a9b9aeca1e92edd5324
J.Reczko <reczko.jakub@gmail.com>
2022-10-14 21:53:42 +0200

    Handle gracefully exit process

--- server/src/index.js
+++ server/src/index.js
@@ -3,5 +3,31 @@ const app = require('./app')
 const PORT = process.env.PORT || 5000
 
 const server = app.listen(PORT, () => {
-    console.log('Server is running', PORT)
+    console.info('Server is running', PORT)
 })
+
+const exitHandler = () => {
+    if (server) {
+        server.close(() => {
+            console.info('Server closed');
+            process.exit(1);
+        });
+    } else {
+        process.exit(1);
+    }
+};
+
+const unexpectedErrorHandler = (error) => {
+    console.error(error);
+    exitHandler();
+};
+
+process.on('uncaughtException', unexpectedErrorHandler);
+process.on('unhandledRejection', unexpectedErrorHandler);
+
+process.on('SIGTERM', () => {
+    console.info('SIGTERM received');
+    if (server) {
+        server.close();
+    }
+});
