c4a6af4676251deb2702199d9f8c34b2bc5cf419
J.Reczko <reczko.jakub@gmail.com>
2022-10-15 09:39:17 +0200

    Update list of api methods

--- web/src/api/events.js
+++ web/src/api/events.js
@@ -1,10 +1,17 @@
-import api from "./base"
+import api from './base'
 
-const list = async () => {
-    const response = await api.get("/v1/events")
-    return response.data
+const list = async () => await api.get('/v1/events')
+const getOne = async (id) => await api.get(`/v1/events/${id}`)
+const create = async (payload) => await api.post('/v1/events', payload)
+const update = async (id, payload) => await api.put(`/v1/events/${id}`, payload)
+const remove = async (id) => await api.delete(`/v1/events/${id}`)
+
+const eventsApi = {
+    list,
+    getOne,
+    create,
+    update,
+    remove
 }
 
-export default {
-    list
-}
+export default eventsApi
