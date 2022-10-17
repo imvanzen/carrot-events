6fbca793e6e30efa125c5567c40928d13a809169
J.Reczko <reczko.jakub@gmail.com>
2022-10-17 09:36:53 +0200

    Event Edit container updates

--- web/src/containers/EventEdit.js
+++ web/src/containers/EventEdit.js
@@ -1,13 +1,36 @@
 import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
 import { Grid } from 'semantic-ui-react'
 import EventForm from '../components/EventForm'
 
 import eventsApi from '../api/events'
 
-const EventEdit = ({ event }) => {
+const EventEdit = () => {
+    const [event, setEvent] = useState(null);
+    let { eventId } = useParams()
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const event = await eventsApi.getOne(eventId)
+                if (!event.data) {
+                    console.log("No event found")
+                }
+                setEvent(event.data)
+            } catch (err) {
+                console.log(err)
+            }
+        }
+
+        fetchData()
+    }, [eventId]);
+
     const onSubmit = async (form) => {
         try {
-            const result = await eventsApi.update(event.id, form)
+            const result = await eventsApi.update(eventId, form)
+            if (!result) {
+                console.log("Update failed")
+            }
         } catch (err) {
             console.log(err.response.data.message)
         }
