f5684d3e6057490f68b0e51cc5e048c63c4070b6
J.Reczko <reczko.jakub@gmail.com>
2022-10-17 09:36:19 +0200

    Event form supports create and edit containers

--- web/src/components/EventForm.js
+++ web/src/components/EventForm.js
@@ -1,28 +1,55 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { Button, Form, Icon, Input } from 'semantic-ui-react'
 import { DateTimeInput } from 'semantic-ui-calendar-react'
 
-const EventForm = ({ onSubmit, event = {
-    first_name: "",
-    last_name: "",
-    email: "",
-    event_date: ""
-} }) => {
+const EventForm = ({ onSubmit, event }) => {
     const [loading, setLoading] = useState(false)
-    const [form, setForm] = useState(event)
+    const [form, setForm] = useState({
+        first_name: "",
+        last_name: "",
+        email: "",
+        event_date: ""
+    })
 
-    // const isFieldValid = (name, val) => {
-    //     const schema = {
-    //         first_name: /^([A-Za-b]+)$/i,
-    //         last_name: /^([A-Za-b]+)$/i,
-    //         email: /^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i,
-    //         event_date: /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}(?:\.\d*)?)((-(\d{2}):(\d{2})|Z)?)$/gmi
-    //     }
-    //     return val.match(schema[name])
-    // }
+    useEffect(() => {
+        if (!event) return
+        console.log(event)
+        setForm(event)
+    }, [event])
+
+    const hasErrors = (name) => {
+        const errors = {
+            first_name: {
+                content: 'Please enter a valid first name',
+                pointing: 'below',
+            },
+            last_name: {
+                content: 'Please enter a valid last name address',
+                pointing: 'below',
+            },
+            email: {
+                content: 'Please enter a valid email address',
+                pointing: 'below',
+            },
+            event_date: {
+                content: 'Please enter a valid event date',
+                pointing: 'below',
+            },
+        }
+        const schema = {
+            first_name: /^([A-Za-b]+)$/i,
+            last_name: /^([A-Za-b]+)$/i,
+            email: /^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i,
+            event_date: /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}(?:\.\d*)?)((-(\d{2}):(\d{2})|Z)?)$/gmi
+        }
+        if (!form[name]) return
+        if (form[name].trim().match(schema[name])) return
+        return errors[name]
+    }
 
     const onDateTimeChange = (event, { name, value }) => {
+        console.log({ name, value })
         setForm({
             ...form,
             [name]: value
@@ -43,25 +70,6 @@ const EventForm = ({ onSubmit, event = {
         setLoading(false)
     }
 
-    const errors = {
-        first_name: {
-            content: 'Please enter a valid email address',
-            pointing: 'below',
-        },
-        last_name: {
-            content: 'Please enter a valid email address',
-            pointing: 'below',
-        },
-        email: {
-            content: 'Please enter a valid email address',
-            pointing: 'below',
-        },
-        event_date: {
-            content: 'Please enter a valid email address',
-            pointing: 'below',
-        },
-    }
-
     return (
         <Form onSubmit={onFormSubmit} loading={loading}>
             <Form.Group widths='equal'>
@@ -72,7 +80,7 @@ const EventForm = ({ onSubmit, event = {
                     value={form.first_name}
                     label='First name'
                     placeholder='First name'
-                    error={errors.email}
+                    error={hasErrors('first_name')}
                 />
                 <Form.Field
                     id='form-input-control-last-name'
@@ -81,7 +89,7 @@ const EventForm = ({ onSubmit, event = {
                     value={form.last_name}
                     label='Last name'
                     placeholder='Last name'
-                    error={errors.email}
+                    error={hasErrors('last_name')}
                 />
             </Form.Group>
             <Form.Field
@@ -91,7 +99,7 @@ const EventForm = ({ onSubmit, event = {
                 value={form.email}
                 label='Email'
                 placeholder='joe@schmoe.com'
-                error={errors.email}
+                error={hasErrors('email')}
             />
             <Form.Field
                 id='form-input-control-event-date'
@@ -106,7 +114,7 @@ const EventForm = ({ onSubmit, event = {
                 startMode='month'
                 clearable
                 closable
-                error={errors.email}
+                error={hasErrors('event_date')}
             />
             <Button.Group floated='right'>
                 <Link to='/'>
