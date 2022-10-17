d646b7d4dff7f5e13479c1cc4fdd5b7ea62d280b
J.Reczko <reczko.jakub@gmail.com>
2022-10-17 12:18:42 +0200

    Navigate to the main list after event creation

--- web/src/containers/EventCreate.js
+++ web/src/containers/EventCreate.js
@@ -1,18 +1,21 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 import { Grid } from 'semantic-ui-react'
 import EventForm from '../components/EventForm'
 
 import eventsApi from '../api/events'
 
 const EventCreate = () => {
+    const navigate = useNavigate();
     const onSubmit = async (form) => {
         try {
             const result = await eventsApi.create(form)
             if (!result) {
                 console.log("Create failed")
             }
+            navigate('/')
         } catch (err) {
-            console.log(err.response.data.message)
+            console.error(err)
         }
     }
 
