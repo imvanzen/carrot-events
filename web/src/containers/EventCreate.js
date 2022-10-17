f991551c8a299746105a92b0cc0572bcf1299bff
J.Reczko <reczko.jakub@gmail.com>
2022-10-17 09:36:39 +0200

    Event Create container updates

--- web/src/containers/EventCreate.js
+++ web/src/containers/EventCreate.js
@@ -1,13 +1,16 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { Grid } from 'semantic-ui-react'
 import EventForm from '../components/EventForm'
 
 import eventsApi from '../api/events'
 
-const EventCreate = ({ event }) => {
+const EventCreate = () => {
     const onSubmit = async (form) => {
         try {
             const result = await eventsApi.create(form)
+            if (!result) {
+                console.log("Create failed")
+            }
         } catch (err) {
             console.log(err.response.data.message)
         }
